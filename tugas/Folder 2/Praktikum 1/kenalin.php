3ef8631b677116b51cdd3468813c502c2541c3f7
sofanjitok <sofanjitok@gmail.com>
2020-10-21 21:40:25 +0700

    tugas wp2

---
+++ tugas/Folder 2/Praktikum 1/kenalin.php
@@ -0,0 +1,11 @@
+<?php
+class kenalin extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalan</h1>";
+        echo "Sofanji<br>
+                saya tinggal di Padaharja<br>
+                saya suka olahraga memancing";
+    }
+}
